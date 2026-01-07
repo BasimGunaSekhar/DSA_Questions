a6066d259ae8dc2aa81095cc3a59dd1cca01d936
BasimGunaSekhar <basimgunasekhar@gmail.com>
2026-01-07 20:12:46 +0530

    Add files via upload

---
+++ PlusOne.java
@@ -0,0 +1,22 @@
+package com.easy;
+
+import java.util.Arrays;
+
+public class PlusOne {
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(plusOne(new int[]{9,9,9})));
+	}
+	public static int[] plusOne(int[] digits) {
+		for (int i = digits.length - 1; i >= 0; i--) {
+			if (digits[i] < 9) {
+				digits[i]++;
+				return digits;
+			} else {
+				digits[i] = 0;
+			}
+		}
+		int[] res = new int[digits.length + 1];
+		res[0] = 1;
+		return res;
+	}
+}
