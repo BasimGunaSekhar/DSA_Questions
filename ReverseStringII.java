68d24e88b4addb2dba4279b804eda652947ab9fd
BasimGunaSekhar <basimgunasekhar@gmail.com>
2026-05-06 20:00:05 +0530

    Add files via upload

---
+++ ReverseStringII.java
@@ -0,0 +1,22 @@
+package com.strings;
+
+public class ReverseStringII {
+	public static void main(String[] args) {
+		System.out.println(reverseStr("abcdefg",2));
+	}
+	public static String reverseStr(String s, int k) {
+		char[] arr = s.toCharArray();
+		for (int i = 0; i < arr.length; i += 2 * k) {
+			int left = i;
+			int right = Math.min(i + k - 1, arr.length - 1);
+			while (left < right) {
+				char temp = arr[left];
+				arr[left] = arr[right];
+				arr[right] = temp;
+				left++;
+				right--;
+			}
+		}
+		return new String(arr);
+	}
+}
